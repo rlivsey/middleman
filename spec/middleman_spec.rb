9a00b769f0dda946cc926b4e6f0c5f615b561e72
Richard Livsey <richard@livsey.org>
2009-05-08 16:11:21 +0200

    move file store specs to bottom of file

--- spec/middleman_spec.rb
+++ spec/middleman_spec.rb
@@ -1,6 +1,74 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'pp'
 
+describe Middleman do
+  
+  def do_get
+    Twitter::Search.new('bacon')
+  end
+  
+  describe "default options" do
+    
+    it "should not be verbose" do
+      Middleman.options[:verbose].should == false
+    end
+    
+    it "should be a hash store" do
+      Middleman.options[:store].class.should == Hash
+    end
+    
+    it "should not have a logger" do
+      Middleman.options[:logger].should be_nil
+    end
+    
+  end
+  
+  describe "with file store" do
+    
+    before :all do
+      Middleman.options[:store] = Middleman::Store::File.new('cache')
+    end    
+        
+  end
+  
+  describe "with no store" do
+    before :all do
+      Middleman.options[:store] = nil
+    end
+    
+    it "should do nothing" do
+    end
+    
+  end
+  
+  describe "with a Hash store" do
+    
+    before :all do
+      Middleman.options[:store] = {}
+    end
+    
+    it "should cache to the hash" do
+    end
+    
+  end
+  
+  describe "with verbose logging" do
+    
+    before :all do
+      Middleman.options[:verbose] = true
+      Middleman.options[:logger]  = DummyLogger.new
+    end
+    
+    it "should log to the logger" do
+    end
+    
+  end
+  
+end
+
+
+
+
 describe Middleman::Store::File do
   
   before :each do
@@ -44,71 +112,6 @@ describe Middleman::Store::File do
     it "should create a file with the MD5 of the name" do
       @store["test"] = 'test'
       File.exist?("#{@store.path}/test").should == true
-    end
-    
-  end
-  
-end
-
-describe Middleman do
-  
-  def do_get
-    Twitter::Search.new('bacon')
-  end
-  
-  describe "with default options" do
-    
-    it "should not be verbose" do
-      Middleman.options[:verbose].should == false
-    end
-    
-    it "defaults to a hash store" do
-      Middleman.options[:store].class.should == Hash
-    end
-    
-    it "should not have a logger set" do
-      Middleman.options[:logger].should be_nil
-    end
-    
-  end
-  
-  describe "with file store" do
-    
-    before :all do
-      Middleman.options[:store] = Middleman::Store::File.new('cache')
-    end    
-        
-  end
-  
-  describe "with no store" do
-    before :all do
-      Middleman.options[:store] = nil
-    end
-    
-    it "should do nothing" do
-    end
-    
-  end
-  
-  describe "with a Hash store" do
-    
-    before :all do
-      Middleman.options[:store] = {}
-    end
-    
-    it "should cache to the hash" do
-    end
-    
-  end
-  
-  describe "with verbose logging" do
-    
-    before :all do
-      Middleman.options[:verbose] = true
-      Middleman.options[:logger]  = DummyLogger.new
-    end
-    
-    it "should log to the logger" do
     end
     
   end
